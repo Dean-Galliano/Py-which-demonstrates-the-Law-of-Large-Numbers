208e3829764c5090bc83d127444c60a096b48d69
Dean Galliano <113016756+Dean-Galliano@users.noreply.github.com>
2022-11-08 15:15:57 +0900

    Py-which-demonstrates-the-Law-of-Large-Numbers
    You can change the rate by changing the value of num.

---
+++ Mainpy.py
@@ -0,0 +1,20 @@
+import random
+
+num=50
+zero=0
+one=0
+rate_zero=0
+rate_one=0
+
+for i in range(num):
+    result=random.randint(0,1)
+    if result==0:
+        zero+=1
+    elif result==1:
+        one+=1
+    rate_zero=(zero/num)*100
+    rate_one=(one/num)*100
+        
+        
+print(f"Done, zero: {zero},one: {one}")
+print(f"Rates are zero:{rate_zero}% and one: {rate_one}%")
